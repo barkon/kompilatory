1686f04821812720f1469dd6dfd1f4422a66bcb2
Bartek <bpkonieczny@gmail.com>
2018-04-08 20:16:12 +0200

    added main

---
+++ parser/main.py
@@ -0,0 +1,18 @@
+import sys
+import ply.yacc as yacc
+import scanner
+import MParser
+
+if __name__ == '__main__':
+
+    try:
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
+        file = open(filename, "r")
+    except IOError:
+        print("Cannot open {0} file".format(filename))
+        sys.exit(0)
+
+    MParser = MParser.MParser()
+    parser = yacc.yacc(module=MParser)
+    text = file.read()
+    parser.parse(text, lexer=scanner.lexer)
