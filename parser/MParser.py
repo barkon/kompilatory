3225a484629e58dcc1689b189a7a68069d188bbf
Bartek <bpkonieczny@gmail.com>
2018-04-08 18:43:09 +0200

    added more classes, and productions

--- parser/MParser.py
+++ parser/MParser.py
@@ -13,10 +13,11 @@ class MParser(object):
 
     precedence = (
        ('nonassoc', 'IFX'),
+       ('nonassoc', 'ELSE'),
        ('left', 'DOTPLUS', 'DOTMINUS'),
        ('left', 'DOTMUL', 'DOTDIV'),
-       ("left", '+', '-'),
-       ("left", '*', '/'),
+       ('left', '+', '-'),
+       ('left', '*', '/'),
     )
 
     def p_error(self, p):
@@ -28,10 +29,12 @@ class MParser(object):
     def p_program(self, p):
         """program : instructions_opt"""
         #print('program:', p)
+        p[0] = data.Program(p[1])
 
     def p_instructions_opt_1(self, p):
         """instructions_opt : instructions """
         #print('instructions_opt_1:', p)
+        p[0] = data.InstructionsOpt(p[1])
 
     def p_instructions_opt_2(self, p):
         """instructions_opt : """
@@ -56,8 +59,9 @@ class MParser(object):
                        | break_instr
                        | continue_instr
                        | return_instr
+                       | print_instr
                        | complex_instr
-                       | assignment"""
+                       | assignment ';'"""
         p[0] = p[1]
         #print('instruction:', p)
 
@@ -68,7 +72,6 @@ class MParser(object):
         #print('if/else instruction:', p)
         p[0] = data.IfElseInstr(p[3], p[5], else_inst)
 
-
     def p_while_inst(self, p):
         """while_instr : WHILE '(' condition ')' instruction"""
         p[0] = data.WhileInstr(p[3], p[5])
@@ -77,58 +80,144 @@ class MParser(object):
     def p_for_inst(self, p):
         """for_instr : FOR for_init instruction"""
         #print('for instruction:', p)
+        p[0] = data.ForInstr(p[2], p[3])
 
     def p_for_init(self, p):
         """for_init : ID '=' INT ':' INT"""
         #print('for init:', p)
+        p[0] = data.ForInit(p[1], p[3], p[5])
 
     def p_break_inst(self, p):
         """break_instr : BREAK ';'"""
         #print('break:', p)
+        p[0] = data.BreakInstr()
 
     def p_continue_inst(self, p):
         """continue_instr : CONTINUE ';'"""
         #print('continue:', p)
+        p[0] = data.ContinueInstr()
 
     def p_return_instr(self, p):
         """return_instr : RETURN expression ';'"""
         #print('return:', p)
+        p[0] = data.ReturnInstr(p[2])
+
+    def p_print_instr(self, p):
+        """print_instr : PRINT STRING ';'"""
+        #print('print:', p)
+        p[0] = data.PrintInstr(p[2])
 
     def p_complex_instr(self, p):
         """complex_instr : '{' instructions '}'"""
         #print('complex:', p)
         p[0] = data.InstructionList
 
-    def p_const(self, p):
-        """const : INT
-                  | FLOAT
-                  | STRING"""
+    def p_number(self, p):
+        """number : INT
+                  | FLOAT"""
+        #print('number:', p)
         p[0] = p[1]
-        #print('const', p)
+
+    def p_const(self, p):
+        """const : number
+                 | STRING"""
+        #print('const:', p)
+        p[0] = data.Const(p[1])
+
+    def p_var_id(self, p):
+        """var_id : ID
+                  | ID '[' INT ']'"""
+        #print('var id:', p)
+        p[0] = data.VarId(p[1], p[3]) if len(p) > 2 else data.VarId(p[1])
 
     def p_assignment(self, p):
-        """assignment : ID '=' expression ';'"""
+        """assignment : var_id '=' expression
+                      | op_assignment"""
         #print('assignment:', p)
         p[0] = data.AssignmentInstr(p[1], p[3])
 
-
     def p_expression(self, p):
-        """expression : ID
+        """expression : var_id
                       | const
-                      | initialization
+                      | matrix_expr
+                      | number_op
+                      | '-' expression
                       | '(' expression ')'"""
         #print('expression:', p)
+        p[0] = p[1]
 
-    def p_initialization(self, p):
-        """initialization : eye"""
-        #print('initialization:', p)
+    def p_matrix_expr(self, p):
+        """matrix_expr : matrix_init
+                       | matrix_op"""
+        #print('matrix expr:', p)
+        p[0] = p[1]
 
-    def p_condition(self, p):
-        """condition : expression"""
-        #print('condition:', p)
+    def p_matrix_init(self, p):
+        """matrix_init : eye
+                       | ones
+                       | zeros
+                       | '[' matrix_rows ']'"""
+        #print('matrix init:', p)
+        p[0] = p[1] if len(p) > 2 else p[2]
 
     def p_eye(self, p):
-        """eye : EYE '(' INT ')'"""
+        """eye : EYE '(' INT ')' ';'"""
         #print('eye:', p)
+        p[0] = data.EyeInit(p[3])
 
+    def p_ones(self, p):
+        """ones : ONES '(' INT ')' ';'"""
+        #print('ones:', p)
+        p[0] = data.OnesInit(p[3])
+
+    def p_zeros(self, p):
+        """zeros : ZEROS '(' INT ')' ';'"""
+        #print('zeros:', p)
+        p[0] = data.ZerosInit(p[3])
+
+    def p_matrix_rows(self, p):
+        """matrix_rows : row_elems
+                       | row_elems ';' matrix_rows"""
+        #print('matrix rows:', p)
+        
+
+    def p_row_elems(self, p):
+        """row_elems : number
+                     | number ',' row_elems"""
+        #print('rows elems:', p)
+
+    def p_matrix_op(self, p):
+        """matrix_op : matrix_expr DOTPLUS matrix_expr
+                     | matrix_expr DOTMINUS matrix_expr
+                     | matrix_expr DOTMUL matrix_expr
+                     | matrix_expr DOTDIV matrix_expr
+                     | matrix_expr '\\''"""
+        #print('matrix op:', p)]
+        p[0] = data.MatrixOp(p[2], [p[1], p[3]]) if len(p) == 4 else data.MatrixOp(p[2], p[1])
+
+    def p_number_op(self, p):
+        """number_op : expression '+' expression
+                     | expression '-' expression
+                     | expression '*' expression
+                     | expression '/' expression"""
+        #print('number op:', p)
+        p[0] = data.MatrixOp(p[2], [p[1], p[3]])
+
+    def p_op_assignment(self, p):
+        """op_assignment : var_id PLUSASSIGN expression
+                         | var_id MINUSASSIGN expression
+                         | var_id MULASSIGN expression
+                         | var_id DIVASSIGN expression"""
+        #print('op assignment:', p)
+        p[0] = data.OpAssignmentInstr(p[1], p[2], p[3])
+
+    def p_condition(self, p):
+        """condition : expression '<' expression
+                     | expression '>' expression
+                     | expression EQ expression
+                     | expression LESSEQ expression
+                     | expression MOREEQ expression
+                     | expression NEQ expression"""
+        #print('condition:', p)
+        p[0] = data.Condition(p[1], p[2], p[3])
 
--- parser/data.py
+++ parser/data.py
@@ -12,14 +12,20 @@ class Float(Const):
     pass
 
 
-class Id(Const):
-    pass
+class VarId(object):
+    def __init__(self, name, index=None):
+        self.index = index
+        self.name = name
 
 
-class Operation(object):
-    def __init__(self, operator, args):
-        self.operator = operator
-        self.args = args
+class Program(object):
+    def __init__(self, instructions_opt):
+        self.instructions_opt = instructions_opt
+
+
+class InstructionsOpt(object):
+    def __init__(self, instructions):
+        self.instructions = instructions
 
 
 class InstructionList(object):
@@ -33,6 +39,13 @@ class InstructionList(object):
 class AssignmentInstr(object):
     def __init__(self, name, expr):
         self.name = name
+        self.expr = expr
+
+
+class OpAssignmentInstr(object):
+    def __init__(self, name, op, expr):
+        self.name = name
+        self.op = op
         self.expr = expr
 
 
@@ -75,6 +88,11 @@ class ReturnInstr(object):
         self.ret = ret
 
 
+class PrintInstr(object):
+    def __init__(self, to_print):
+        self.to_print = to_print
+
+
 class ComplexInstr(object):
     def __init__(self, instructions):
         self.instructions = instructions
@@ -95,3 +113,24 @@ class ZerosInit(object):
         self.size = size
 
 
+class Operation(object):
+    def __init__(self, operator, args):
+        self.operator = operator
+        self.args = args
+
+
+class MatrixOp(Operation):
+    def __init__(self, operator, args):
+        super().__init__(operator, args)
+
+
+class NumberOp(Operation):
+    def __init__(self, operator, args):
+        super().__init__(operator, args)
+
+
+class Condition(object):
+    def __init__(self, larg, op, rarg):
+        self.larg = larg
+        self.op = op
+        self.rarg = rarg
