a4a0932f9335e38d8f2ee263434e79be3da89919
Bartek <bpkonieczny@gmail.com>
2018-04-22 13:10:53 +0200

    half of printTree

---
+++ parser/TreePrinter.py
@@ -0,0 +1,76 @@
+import data
+
+INDENT_TOKEN = '| '
+
+
+def addToClass(cls):
+    def decorator(func):
+        setattr(cls, func.__name__, func)
+        return func
+    return decorator
+
+
+class TreePrinter:
+
+    @addToClass(data.BreakInstr)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + 'BREAK\n'
+
+    @addToClass(data.ContinueInstr)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + 'CONTINUE\n'
+
+    @addToClass(data.ReturnInstr)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + 'RETURN\n' + self.ret.printTree(indent+1)
+
+    @addToClass(data.PrintInstr)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + 'PRINT\n' + self.to_print.printTree(indent+1)
+
+    @addToClass(data.InstrBlock)
+    def printTree(self, indent=0):
+        return "" if self.instructions is None else self.instructions.printTree(indent+1)
+
+    @addToClass(data.EyeInit)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + 'EYE\n' + self.size.printTree(indent+1)
+
+    @addToClass(data.OnesInit)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + 'ONES\n' + self.size.printTree(indent+1)
+
+    @addToClass(data.ZerosInit)
+    def printTree(self, indent=0):
+        return INDENT_TOKEN * indent + 'ZEROS\n' + self.size.printTree(indent+1)
+
+    @addToClass(data.BinOperation)
+    def printTree(self, indent=0):
+        return self.operator.printTree(indent) + self.larg.printTree(indent+1) + self.rarg.printTree(indent+1)
+
+    @addToClass(data.UnOperation)
+    def printTree(self, indent=0):
+        return self.operator.printTree(indent) + self.arg.printTree(indent+1)
+
+    @addToClass(data.PrintVarsList)
+    def printTree(self, indent=0):
+        ret = ''
+        for i in range(len(self.print_list)):
+            ret += self.print_list[i].printTree(indent)
+        return ret
+
+    @addToClass(data.MatrixRows)
+    def printTree(self, indent=0):
+        ret = INDENT_TOKEN * indent + '[\n'
+        for i in range(len(self.rows)):
+            ret += self.rows[i].printTree(indent+1)
+        ret += INDENT_TOKEN * indent + ']\n'
+        return ret
+
+    @addToClass(data.MatrixRow)
+    def printTree(self, indent=0):
+        ret = INDENT_TOKEN * indent + '[\n'
+        for i in range(len(self.row)):
+            ret += self.row[i].printTree(indent+1)
+        ret += INDENT_TOKEN * indent + ']\n'
+        return ret
